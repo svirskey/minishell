7ca6a6a93f92c8a6aa89bce081da2abc2cb99272
ruslan.svr@mail.ru <ruslansvr@mail.ru>
2022-07-24 19:47:57 +0300

    fix parser =)

--- parser.c
+++ parser.c
@@ -6,6 +6,18 @@
 // add separators between words for this case : 'e''c''h''o' == echo (correct)
 // echo asd'asd  -> unclosed quotes are not interpretating
 // echo $PWD != echo $P'W'D => read till spec symbol
+static int till_sep(char *str, int begin)
+{
+    int c;
+
+	c = begin;
+	if (!str)
+		return 0;
+	while (str[c] && str[c] != '$')
+		c++;
+	return c - begin;
+}
+
 
 static void opening(t_info *info)
 {
@@ -28,27 +40,29 @@ static void opening(t_info *info)
             }
             else
             {
-                i = next_char(tmp->value, 0, '$');
-                printf("%d\n", i);
-                lst_push_back(&opn, lst_new(ft_strdup("word"), ft_substr(tmp->value, 0, i)));
-                i++;
-                while (next_char(tmp->value, i, '$') != -1)
+                i = 0
+                while (str[i])
                 {
                     if (((char *)tmp->value)[i] == '$')
                     {
-                        //TODO
                         i++;
-                    }
-                    else if (((char *)tmp->value)[i] == '?')
-                    {
-                        //TODO
+                        if (!((char *)tmp->value)[i])
+                            lst_push_back(&opn, lst_new(ft_strdup("word"),ft_strdup("$")));
+                        if (((char *)tmp->value)[i] == '$')
+                        {
+                             lst_push_back(&opn, lst_new(ft_strdup("pid"), ft_strdup("$"))); // todo itoa
+                        }
+                        else if (((char *)tmp->value)[i] == '?')
+                        {
+                            lst_push_back(&opn, lst_new(ft_strdup("exit"), ft_strdup("?"))); // todo itoa 
+                        }
+                        else
+                            lst_push_back(&opn, lst_new(ft_strdup("word"), lst_get_value(info->envp_list, ft_substr(tmp->value, i, till_sep(tmp->value, i)))));
                     }
                     else
-                        lst_push_back(&opn, lst_new(ft_strdup("word"), lst_get_value(info->envp_list, ft_substr(tmp->value, i, (next_char(tmp->value, i, '$'))))));
-                    i += next_char(tmp->value, i, '$') + 1;
+                        lst_push_back(&opn, lst_new(ft_strdup("word"), ft_substr(tmp->value, i, till_sep(tmp->value, i)))); 
+                    i += till_sep(tmp->value, i) + 1;
                 }
-                if (((char *)tmp->value)[i - 2] && ((char *)tmp->value)[i - 2] != '$' && ((char *)tmp->value)[i - 1] == '$')
-                    lst_push_back(&opn, lst_new(ft_strdup("word"),ft_strdup("$")));
                 prev->next = opn;
                 while (opn->next)
                     opn = opn->next;
